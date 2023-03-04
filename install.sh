e562305566dfe3a0a9c97031689b6790f55d6708
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-04 22:15:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ wget -O main.py https://gitcode.net/qq_32394351/dr_py/-/raw/master/app.py
 wget https://gitcode.net/qq_32394351/dr_py/-/archive/master/dr_py-master.zip
 unzip dr_py-master.zip && mv dr_py-master/* .
 pip install -r requirements.txt
+pip install flask_migrate
+pip install flask==2.2.2
+pip install flask-sqlalchemy==2.5.1
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
- rm -rf dr_py-master/ && rm -rf install.sh
+rm -rf dr_py-master/ && rm -rf drpy-on-replit/
+rm -rf install.sh
