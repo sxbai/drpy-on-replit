8f35e556f0cbfe2c006d86d096c1f14ce6294c8f
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-04 22:18:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 echo "drpy一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/drpy-on-replit"
-git clone https://github.com/sxbai/drpy-on-replit.git
+rm -rf public/ && rm -rf templates/
+rm -rf README.md && git clone https://github.com/sxbai/drpy-on-replit.git
 mv drpy-on-replit/* . && cp -r drpy-on-replit/.replit .
 wget -O main.py https://gitcode.net/qq_32394351/dr_py/-/raw/master/app.py
 wget https://gitcode.net/qq_32394351/dr_py/-/archive/master/dr_py-master.zip
