320214ae8b0b8e448f8b1b456999ad2cea1d71d7
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-16 22:33:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,20 @@ echo "drpy一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/drpy-on-replit"
 rm -rf public/ && rm -rf templates/
-rm -rf README.md && git clone https://github.com/sxbai/drpy-on-replit.git
-cp -r drpy-on-replit/.replit . && mv drpy-on-replit/* .
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://github.com/sxbai/drpy-on-replit/raw/master/.replit
+wget -O replit.nix https://github.com/sxbai/drpy-on-replit/raw/master/replit.nix
+cd ..
+wget https://github.com/sxbai/drpy-on-replit/raw/master/main.sh
+wget https://github.com/sxbai/drpy-on-replit/raw/master/fix.sh
 wget https://gitcode.net/qq_32394351/dr_py/-/archive/master/dr_py-master.zip
+nix-env -iA nixpkgs.unzip
 unzip dr_py-master.zip && mv dr_py-master/* .
+cp -r build/.replit . && cp -r build/replit.nix .
 pip install -r requirements.txt
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
-rm -rf dr_py-master/ && rm -rf drpy-on-replit/
-rm -rf dr_py-master.zip && rm -rf install.sh
+rm -rf dr_py-master/ && rm -rf build/
+rm -rf dr_py-master.zip && rm -rf README.md
