e2671b0c976009dccbc5c5fed19238ec0fbd3ca1
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-04-24 07:58:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ wget -O main.py https://github.com/sxbai/drpy-on-replit/raw/master/main.py
 wget https://github.com/sxbai/drpy-on-replit/raw/master/fix.sh
 wget https://github.com/hjdhnx/dr_py/archive/refs/heads/main.zip
 nix-env -iA nixpkgs.unzip
-unzip main.zip && mv dr_py-master/* .
+unzip main.zip && mv dr_py-main/* .
 cp -r build/.replit . && cp -r build/replit.nix .
 pip install -r requirements.txt
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
-rm -rf dr_py-master/ && rm -rf build/
-rm -rf dr_py-master.zip && rm -rf README.md
+rm -rf dr_py-main/ && rm -rf build/
+rm -rf main.zip && rm -rf README.md
