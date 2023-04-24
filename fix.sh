8b1fa1e20e2011d55cbcf8ffe1f97911ab808d44
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-04-24 14:21:48 +0800

    Update fix.sh
    修复drpy

--- fix.sh
+++ fix.sh
@@ -4,4 +4,5 @@ echo "GitHub开源地址：https://github.com/sxbai/drpy-on-replit"
 pip install flask_migrate
 pip install flask==2.2.2
 pip install flask-sqlalchemy==2.5.1
+pip install SQLAlchemy==1.4.41
 echo "恭喜修复drpy完成"
